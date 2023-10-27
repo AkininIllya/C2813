88d02195bdff1593eddf87cd4b791ecba01ab4f4
AkininIllya <akininila@gmail.com>
2023-10-27 14:07:11 +0300

    lesson 4

---
+++ lesson 4/main.py
@@ -0,0 +1,7 @@
+from cat import Cat
+from dog import Dog
+
+murchyk = Cat(nick="Murchyk", age=1.2, breed="Siamese", sound="Meow-meow")
+rokki = Dog(nick="Rokki", age=2.3, breed="German shepher", sound="Woof-woof")
+print(murchyk)
+print(rokki)
